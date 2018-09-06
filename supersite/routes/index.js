ced7e8646f206f001bf15c159b998d54426f4f4c
Harrison <scharr01@corp.westworlds.com>
2018-09-06 14:10:39 -0500

    Added search feature to the site, it works this time

--- supersite/routes/index.js
+++ supersite/routes/index.js
@@ -10,7 +10,7 @@ router.get("/", function(req, res){
   res.render("index");
 });
 
-router.get("/search", function(req, res){y
+router.get("/search", function(req, res){
   if (req.query.search){
     const regex = new RegExp(escapeRegex(req.query.search), '');
     
