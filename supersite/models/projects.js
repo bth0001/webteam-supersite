c6655192ce15020c91e415e1db921609eae7323f
Harrison <scharr01@corp.westworlds.com>
2018-09-27 15:45:48 -0500

    Project Update

--- supersite/models/projects.js
+++ supersite/models/projects.js
@@ -1,28 +1,51 @@
 const mongoose = require("mongoose");
 
 const projectSchema = new mongoose.Schema({
-   name: String,
-   description: String,
-   date: String,
-   status: String,
-   author: {
-      id: {
-         type: mongoose.Schema.Types.ObjectId,
-         ref: "User"
-      },
-      firstName: String,
-      email: String
-   },
-   owner: {
-       type: mongoose.Schema.Types.ObjectId,
-       ref: "User"
-   },
-   comments: [
-      {
-         type: mongoose.Schema.Types.ObjectId,
-         ref: "ProjectComments"
-      }
-   ]
-}, {timestamps: {createdAt: 'created_at'}});
+    name: String,
+    description: String,
+    date: String,
+    status: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        firstName: String,
+        email: String
+    },
+    owner: [{
+        firstName: {
+            type: String
+        },
+        email: {
+            type: String
+        },
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    }],
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ProjectComments"
+    }],
+    history: [{
+        historyName: {
+            type: String
+        },
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        time: {
+            type: Date,
+            default: Date.now
+        }
+    }],
+}, {
+    timestamps: {
+        createdAt: 'created_at'
+    }
+});
 
 module.exports = mongoose.model("Projects", projectSchema);
--- supersite/routes/projects.js
+++ supersite/routes/projects.js
@@ -23,9 +23,16 @@ router.post("/", function(req, res){
     var status = req.body.status;
     var date = req.body.date;
     var desc = req.body.description;
+    var owner = req.body.owner;
     var author = {id: req.user._id, firstName: req.user.firstName, email: req.user.email};
     var newProject = {name: name, status: status, date: date, description: desc, author: author};
+
+    console.log("-------owner-----")
+    console.log(owner.length);
+
     Project.create(newProject, function(err, newlyCreatedProject){
+        console.log("-------NEW PROJECT-----")
+        console.log(newlyCreatedProject);
         if(err){
             console.log(err);
         } else {
@@ -61,23 +68,51 @@ router.get("/:id", function(req, res){
 });
 
 //edit projects route
-router.get("/:id/edit", middleware.checkProjectOwnership, function(req, res){
+router.get("/:id/edit", function(req, res){
         Project.findById(req.params.id, function(err, foundProject){
                     res.render("projects/edit", {project: foundProject, moment: moment});
 });
 });
 //update projects route
-router.put("/:id", middleware.checkProjectOwnership, function(req, res){
-   //find and update correct project
-   Project.findByIdAndUpdate(req.params.id, req.body.project, function(err, updatedProject){
-       if(err){
-           res.redirect("/projects");
-       } else {
-           req.flash("success", "You have successfully updated the project");
-           res.redirect("/projects/" + req.params.id);
-       }
-   });
-});
+// router.put("/:id", middleware.checkProjectOwnership, function(req, res){
+//    //find and update correct project
+//    Project.findByIdAndUpdate(req.params.id, req.body.project, function(err, updatedProject){
+//        if(err){
+//            res.redirect("/projects");
+//        } else {
+//            req.flash("success", "You have successfully updated the project");
+//            res.redirect("/projects/" + req.params.id);
+//        }
+//    });
+// });
+
+/////////////////////////////////////
+
+router.put("/:id", function(req, res){
+  
+    var historyArray = [];
+    Project.findById(req.params.id, function(err, foundProject){
+      for(i=0; i < foundProject.history.length; i++){
+        historyArray.push(foundProject.history[i]);
+      }
+      var history = {historyName: req.user.firstName, id: req.user._id};
+      historyArray.push(history);
+     //find and update correct project
+     var projectTracking = req.body.project;
+     const newProject = Object.assign(projectTracking, {history: historyArray});
+      Project.findByIdAndUpdate(req.params.id, newProject, function(err, updatedProject){
+        if(err){
+            req.flash("error", err.message);
+            res.redirect("/projects");
+         } else {
+             req.flash("success", "You have successfully updated the Project");
+             res.redirect("/projects/" + req.params.id);
+         }
+     });
+    });
+    });
+
+    ///////////////////////////
 
 
 //Destroy Route
