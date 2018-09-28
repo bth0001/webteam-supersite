5347bbe60d3089779a409d9a35b1e562caf627c3
haney2121 <haney037332@gmail.com>
2018-09-28 08:05:27 -0500

    updated Tracker forms styles and setup url tracker Page

--- supersite/models/teamTracker.js
+++ supersite/models/teamTracker.js
@@ -5,90 +5,99 @@ const TaskType = require("./taskType");
 // var UserSchema = require('mongoose').model('User').schema;
 // var TaskTypeSchema = require('mongoose').model('TaskType').schema;
 
-const teamTrackerSchema = new mongoose.Schema({
-  author: {
-    id: {
-      type: mongoose.Schema.Types.ObjectId,
+const teamTrackerSchema = new mongoose.Schema(
+  {
+    author: {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      },
+      firstName: String,
+      email: String
+    },
+    date: {
+      type: String
+    },
+    acctNum: {
+      type: Number,
+      required: true
+    },
+    buildPkg: {
+      type: String
+    },
+    starterTemplate: {
+      type: String,
+      default: null
+    },
+    specialFeatures: {
+      type: String,
+      default: null
+    },
+    domain: {
+      type: String
+    },
+    cssPath: {
+      type: String,
+      default: null
+    },
+    server: {
+      type: String
+    },
+    notes: {
+      type: String
+    },
+    homeSS: {
+      type: String
+    },
+    interiorSS: {
+      type: String
+    },
+    designer: {
+      type: String,
       ref: "User"
     },
-    firstName: String,
-    email: String
-  },
-  date: {
-    type: String
-  },
-  acctNum: {
-    type: Number,
-    required: true
-  },
-  buildPkg: {
-    type: String
-  },
-  starterTemplate: {
-    type: String,
-    default: null
-  },
-  specialFeatures: {
-    type: String,
-    default: null
-  },
-  domain: {
-    type: String
-  },
-  cssPath: {
-    type: String,
-    default: null
-  },
-  server: {
-    type: String
-  },
-  notes: {
-    type: String
-  },
-  homeSS: {
-    type: String
-  },
-  interiorSS: {
-    type: String
-  },
-  designer: {
+    onboarder: {
       type: String,
       ref: "User"
-  },
-  onboarder: {
-      type: String,
-      ref: "User"
-  },
-  archive: {
+    },
+    archive: {
       type: Boolean,
-      default: false,
+      default: false
+    },
+    history: [
+      {
+        historyName: {
+          type: String
+        },
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+        },
+        time: {
+          type: Date,
+          default: Date.now
+        }
+      }
+    ],
+    taskTypes: [
+      {
+        taskTypeName: {
+          type: String,
+          required: true
+        },
+        quantity: {
+          type: String,
+          default: null
+        },
+        taskNotes: {
+          type: String,
+          default: "N/a"
+        }
+      }
+    ]
   },
-  history: [{
-    historyName: {
-      type: String
-    },
-    id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
-    },
-    time: { 
-        type : Date, 
-        default: Date.now 
-    }
-  }],
-  taskTypes: [{
-    taskTypeName: {
-      type: String
-    },
-    quantity: {
-      type: String
-    },
-    taskNotes: {
-      type: String
-    }
-  }]
-}, {timestamps: {createdAt: 'created_at'}});
-
+  { timestamps: { createdAt: "created_at" } }
+);
 
 const TeamTracker = mongoose.model("TeamTracker", teamTrackerSchema);
 
