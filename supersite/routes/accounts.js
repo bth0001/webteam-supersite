e493de5faeec38b3d394fe150d5b1b9f35a3b0ea
haney2121 <haney037332@gmail.com>
2018-11-17 00:07:50 -0600

    possible fix for user profile image removed

--- supersite/routes/accounts.js
+++ supersite/routes/accounts.js
@@ -7,14 +7,14 @@ var db = require("../models");
 //----Image Upload Start
 var multer = require("multer");
 var storage = multer.diskStorage({
-  filename: function (req, file, callback) {
+  filename: function(req, file, callback) {
     callback(null, Date.now() + file.originalname); // sets file name
   },
-  destination: function (req, file, cb) {
+  destination: function(req, file, cb) {
     cb(null, "uploads"); //file upload destination
   }
 });
-var imageFilter = function (req, file, cb) {
+var imageFilter = function(req, file, cb) {
   // accept image files only
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
     return cb(new Error("Only image files are allowed!"), false);
@@ -28,19 +28,20 @@ var upload = multer({
 });
 //----Image Upload End
 
-router.get("/", function (req, res, next) {
+router.get("/", function(req, res, next) {
   var perPage = 10;
   var pageQuery = parseInt(req.query.page);
   var pageNumber = pageQuery ? pageQuery : 1;
   var noMatch = null;
   if (req.query.search) {
-    const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+    const regex = new RegExp(escapeRegex(req.query.search), "gi");
     db.User.find({ firstName: regex })
-      .collation({ locale: 'en', caseLevel: false, caseFirst: "off" }).sort({ isMaster: -1 })
-      .skip((perPage * pageNumber) - perPage)
+      .collation({ locale: "en", caseLevel: false, caseFirst: "off" })
+      .sort({ isMaster: -1 })
+      .skip(perPage * pageNumber - perPage)
       .limit(perPage)
-      .exec(function (err, foundUsers) {
-        db.User.countDocuments({ firstName: regex }).exec(function (err, count) {
+      .exec(function(err, foundUsers) {
+        db.User.countDocuments({ firstName: regex }).exec(function(err, count) {
           if (err) {
             req.flash("error", "No Users Found");
             res.redirect("/accounts");
@@ -49,7 +50,6 @@ router.get("/", function (req, res, next) {
               noMatch = "No users match that search, please try again.";
               req.flash("error", noMatch);
               return res.redirect("/accounts");
-
             }
             res.render("accounts/index", {
               users: foundUsers,
@@ -63,26 +63,31 @@ router.get("/", function (req, res, next) {
       });
   } else {
     // get all campgrounds from DB
-    db.User.find({}).collation({ locale: 'en', caseLevel: false, caseFirst: "off" }).sort({ isMaster: -1 }).skip((perPage * pageNumber) - perPage).limit(perPage).exec(function (err, foundUsers) {
-      db.User.countDocuments().exec(function (err, count) {
-        if (err) {
-          console.log(err);
-        } else {
-          res.render("accounts/index", {
-            users: foundUsers,
-            current: pageNumber,
-            pages: Math.ceil(count / perPage),
-            noMatch: noMatch,
-            search: false
-          });
-        }
+    db.User.find({})
+      .collation({ locale: "en", caseLevel: false, caseFirst: "off" })
+      .sort({ isMaster: -1 })
+      .skip(perPage * pageNumber - perPage)
+      .limit(perPage)
+      .exec(function(err, foundUsers) {
+        db.User.countDocuments().exec(function(err, count) {
+          if (err) {
+            console.log(err);
+          } else {
+            res.render("accounts/index", {
+              users: foundUsers,
+              current: pageNumber,
+              pages: Math.ceil(count / perPage),
+              noMatch: noMatch,
+              search: false
+            });
+          }
+        });
       });
-    });
   }
 });
 
-router.get("/:id", function (req, res, next) {
-  db.User.findById(req.params.id, function (err, foundUser) {
+router.get("/:id", function(req, res, next) {
+  db.User.findById(req.params.id, function(err, foundUser) {
     if (err) {
       req.flash("error", "User does not exist");
       res.redirect("/accounts");
@@ -91,8 +96,8 @@ router.get("/:id", function (req, res, next) {
   });
 });
 
-router.post("/:id", upload.single("image"), function (req, res) {
-  db.User.findById(req.params.id, function (err, sanitizedUser) {
+router.post("/:id", upload.single("image"), function(req, res) {
+  db.User.findById(req.params.id, function(err, sanitizedUser) {
     if (!sanitizedUser) {
       req.flash("error", "No account found");
       return res.redirect("/accounts");
@@ -112,7 +117,7 @@ router.post("/:id", upload.single("image"), function (req, res) {
     }
     // Check to see if image was inserted
     if (req.file === undefined || req.file === null) {
-      profileImageUrl = sanitizedUser.profileImageUrl;
+      profileImageUrl = "/images/Projectweb.png";
     } else {
       req.body.profileImageUrl = req.file.path;
       var profileImageUrl = req.body.profileImageUrl;
@@ -133,8 +138,8 @@ router.post("/:id", upload.single("image"), function (req, res) {
     sanitizedUser.socials = socials;
     sanitizedUser.skills = skills;
     // don't forget to save!
-    sanitizedUser.setPassword(req.body.password, function () {
-      sanitizedUser.save(function (err) {
+    sanitizedUser.setPassword(req.body.password, function() {
+      sanitizedUser.save(function(err) {
         if (err) {
           if (err.name === "MongoError" && err.code === 11000) {
             req.flash("error", "This email address is already in use.");
@@ -149,17 +154,20 @@ router.post("/:id", upload.single("image"), function (req, res) {
   });
 });
 
-router.delete("/:id", function (req, res) {
-  db.User.findByIdAndRemove(req.params.id, function (err, foundUser) {
+router.delete("/:id", function(req, res) {
+  db.User.findByIdAndRemove(req.params.id, function(err, foundUser) {
     if (err) {
       req.flash("error", err);
       res.redirect("/accounts");
     } else {
-      req.flash("success", `${foundUser.firstName}'s account has been deleted.`)
+      req.flash(
+        "success",
+        `${foundUser.firstName}'s account has been deleted.`
+      );
       res.redirect("/accounts");
     }
-  })
-})
+  });
+});
 
 function escapeRegex(text) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
